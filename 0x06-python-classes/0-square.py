a37deb352e59bfa96ea747dfd136fc4174fdb8cf
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-09-14 12:19:15 +0300

    Python - Classes and Objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Class square: defines a square
+"""
+
+class Square:
+    """Empty square class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Class square: Create a square that contains a size
+"""
+
+class Square:
+    """Class Square with private attribute size"""
+    def __init__(self, size):
+        """initialize variables"""
+        self.__size = size
