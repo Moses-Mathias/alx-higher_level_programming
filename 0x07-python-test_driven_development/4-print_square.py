ebb96057f1e54c5122f943a3f6a74d0cfe41a7b0
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-09-21 21:05:36 +0300

    Python - test driven development

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-Progam that print square with # symbol according to input size
+This module is composed by a function that prints a square with the character #
 """
 
 
 def print_square(size):
-    """ function that prints a square with the character # """
-    s = "size must be an integer"
-    if not (isinstance(size, int)):
-        raise TypeError(s)
-    if (size < 0):
+    """ Function that prints a square with the character #
+    Args:
+        size: size of the square printed
+    Returns:
+        No return
+    Raises:
+        TypeError: If size is not an integer number
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
         raise ValueError("size must be >= 0")
-    if (isinstance(size, float) and size < 0):
-        raise TypeError(s)
-    for x in range(size):
-        for y in range(size):
-            print("#", end='')
-        print('')
+
+    for i in range(size):
+        print("#" * size)
