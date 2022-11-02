785c34938c5d47a02566b07ea5b74466a226495b
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-11-02 13:33:10 +0300

    SQL More Queries

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,4 +2,4 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- If a show doesn’t have a genre, display NULL
-SELECT tvs.title, it.genre_id FROM tv_show_genre it RIGHT OUTER JOIN tv_shows tv ON it.show_id = tvs.id ORDER BY tvs.title, it.genre_id ASC;
+SELECT tvs.title, it.genre_id FROM  tv_show_genres it RIGHT OUTER JOIN tv_shows tvs ON it.show_id = tvs.id ORDER BY tvs.title, it.genre_id ASC;
